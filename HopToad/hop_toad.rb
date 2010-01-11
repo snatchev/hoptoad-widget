2b4cdfe6cbee2fcdbc49897b2f2b02481d97c4fd
Nando Vieira <fnando.vieira@gmail.com>
2010-01-11 16:42:47 -0200

    Removing my own API keys... stupid, yes I am!

--- HopToad/hop_toad.rb
+++ HopToad/hop_toad.rb
@@ -9,41 +9,38 @@ class HopToad
   def url
     "http://#{subdomain}.hoptoadapp.com/errors.xml?auth_token=#{api_key}&page=#{page}"
   end
-  
+
   def api_key
     CGI.escape ENV["API_KEY"]
   end
-  
+
   def subdomain
     CGI.escape ENV["SUBDOMAIN"]
   end
-  
+
   def page
     [ENV["PAGE"].to_i, 1].max.to_s
   end
-  
+
   def load
     @load ||= open(url).read rescue nil
   end
-  
+
   def load!
     @load = nil
     load
   end
-  
+
   def authorized?
     !(load.to_s =~ /<!DOCTYPE html PUBLIC/m)
   end
 end
 
-ENV["SUBDOMAIN"] = "fnando"
-ENV["API_KEY"] = "6d165a6b1ca34e7c76374be75221ff930bd6cb96"
-
 ht = HopToad.new
 
 if ht.authorized?
   xml = REXML::Document.new(ht.load)
-  
+
   if xml.elements.to_a("groups/group").size.zero?
     contents = "no-results"
   else
@@ -54,7 +51,7 @@ if ht.authorized?
       count       = group.elements["notices-count"].text.to_i
       most_recent = Time.parse(group.elements["most-recent-notice-at"].text)
       id          = group.elements["id"].text.to_i
-    
+
       contents << %(
         <p onclick="widget.openURL('http://#{ht.subdomain}.hoptoadapp.com/errors/#{id}');" title="Go to HopToad" id="exception-#{id}" class="exception">
           <strong>#{count}</strong>
@@ -66,6 +63,6 @@ if ht.authorized?
       )
     }
   end
-  
+
   puts contents
 end
